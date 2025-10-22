e0afc35b48d3124fe95f6110d77b3e30f02510ca
joshua camilo <joshcamilo111@gmail.com>
2025-10-22 19:45:16 +0100

    Update on Chat but not tested yet

--- backend/languifyBackend/src/main/java/org/rest/languifybackend/Chat/Controller/ChatController.java
+++ backend/languifyBackend/src/main/java/org/rest/languifybackend/Chat/Controller/ChatController.java
@@ -1,4 +1,75 @@
 package org.rest.languifybackend.Chat.Controller;
 
-public class ChatController {
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.rest.languifybackend.Chat.Model.Chat;
+import org.rest.languifybackend.Chat.Model.ChatRecord;
+import org.rest.languifybackend.Chat.Model.Direction;
+import org.rest.languifybackend.Chat.Service.ChatService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/languify/chat")
+@RequiredArgsConstructor
+public class ChatController
+{
+    private final ChatService chatService;
+
+    @PostMapping("/createOrGet")
+    public ResponseEntity<Chat>  createOrGet(@RequestParam Long userId1, @RequestParam Long userId2, HttpServletRequest request)
+    {
+        String userIp = getClientIp(request);
+
+        Chat chat = chatService.createOrgetChat(userId1, userId2, userIp);
+        return ResponseEntity.ok(chat);
+    }
+
+    private String  getClientIp(HttpServletRequest request)
+    {
+        String ip = request.getHeader("X-Forwarded-For");
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)){
+            ip = request.getRemoteAddr();
+        } else {
+            ip = ip.split(",")[0];
+        }
+        return ip;
+    }
+
+    @GetMapping("/getChats/user/{userId}")
+    public ResponseEntity<List<Chat>> getChatsFromUser(@PathVariable Long userId) {
+        List<Chat> chats = chatService.listChatsByUser(userId);
+        return ResponseEntity.ok(chats);
+    }
+
+    @GetMapping("/getMessage/user/{userId}/chat/{chatId}/message/{messageId}")
+    public ResponseEntity<String> getMessage(@PathVariable Long userId, @PathVariable Long chatId, @PathVariable Long messageId){
+
+        String message = chatService.findMessage(userId, chatId, messageId);
+
+        if (message == null) {
+        return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(message);
+
+    }
+
+    @PostMapping("/sendMessage")
+    public ResponseEntity<ChatRecord> sendMessage(
+            @RequestParam Long chatId,
+            @RequestParam String originalText,
+            @RequestParam String translatedText,
+            @RequestParam Direction direction) {
+
+        ChatRecord record = chatService.sendMessage(chatId, originalText, translatedText, direction);
+        return ResponseEntity.ok(record);
+    }
+
+    @DeleteMapping("/deleteChat/{chatId}")
+    public ResponseEntity<String> deleteChat (@PathVariable Long userId, @RequestParam Long chatId) {
+        Chat deletedchat = chatService.deleteChat(userId, chatId);
+        return ResponseEntity.ok("deleted");
+    }
 }
--- backend/languifyBackend/src/main/java/org/rest/languifybackend/Chat/Model/Direction.java
+++ backend/languifyBackend/src/main/java/org/rest/languifybackend/Chat/Model/Direction.java
@@ -1,7 +1,17 @@
 package org.rest.languifybackend.Chat.Model;
 
+
+import lombok.Getter;
+
+@Getter
 public enum Direction
 {
     USER_TO_AI,
-    AI_TO_USER;
+    AI_TO_USER,
+    USER_TO_USER;
+    private final String directiomname;
+    Direction()
+    {
+        this.directiomname = this.name();
+    }
 }
