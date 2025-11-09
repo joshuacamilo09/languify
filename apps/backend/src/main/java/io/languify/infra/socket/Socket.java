2eedc1642ea0cb5b806db13886421d1ca8a7ad5f
cekrauseee <henrique@cekrause.eu>
2025-11-09 13:02:38 +0000

    handler class

--- apps/backend/src/main/java/io/languify/infra/socket/Socket.java
+++ apps/backend/src/main/java/io/languify/infra/socket/Socket.java
@@ -1,9 +1,7 @@
 package io.languify.infra.socket;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
@@ -13,13 +11,10 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 @RequiredArgsConstructor
 public class Socket implements WebSocketConfigurer {
   private final Handshake handshake;
-  private final Handler handler;
+  private final GlobalHandler handler;
 
   @Override
   public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-    registry
-        .addHandler(handler, "/socket")
-        .addInterceptors(this.handshake)
-        .setAllowedOrigins("*");
+    registry.addHandler(handler, "/socket").addInterceptors(this.handshake).setAllowedOrigins("*");
   }
 }
