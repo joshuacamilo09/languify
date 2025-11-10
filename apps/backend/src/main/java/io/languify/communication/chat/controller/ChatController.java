639650e9d950c3e39fbc03a5e0f1a208985d1025
cekrauseee <henrique@cekrause.eu>
2025-11-10 11:05:22 +0000

    wip chat controller

---
+++ apps/backend/src/main/java/io/languify/communication/chat/controller/ChatController.java
@@ -0,0 +1,8 @@
+package io.languify.communication.chat.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/chats")
+class ChatController {}
